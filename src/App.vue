da3f72f413712b63ba135be19ff988894c1667e7
Federico Pizzeghello <pizzeghello.work@gmail.com>
2022-02-23 17:28:15 +0100

    main vue

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <ContainerAlbum></ContainerAlbum>
+  </div>
+</template>
+
+<script>
+import ContainerAlbum from './components/ContainerAlbum.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    ContainerAlbum
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/style/general.scss"
+</style>
